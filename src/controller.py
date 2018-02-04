dc2e9e55a4e6bdf19c867bfe832f1d4893d1d49c
Hristo Dokov <hdokov@gmail.com>
2018-02-04 11:18:12 +0200

    add joystic control test

---
+++ src/controller.py
@@ -0,0 +1,39 @@
+# Outer try / except catches the RobotStopException we just defined, which we'll raise when we want to
+# bail out of the loop cleanly, shutting the motors down. We can raise this in response to a button press
+try:
+    while True:
+        # Inner try / except is used to wait for a controller to become available, at which point we
+        # bind to it and enter a loop where we read axis values and send commands to the motors.
+        try:
+            # Bind to any available joystick, this will use whatever's connected as long as the library
+            # supports it.
+            with ControllerResource(dead_zone=0.1, hot_zone=0.2) as joystick:
+                print('Controller found, press HOME button to exit, use left stick to drive.')
+                print(joystick.controls)
+                # Loop until the joystick disconnects, or we deliberately stop by raising a
+                # RobotStopException
+                while joystick.connected:
+                    # Get joystick values from the left analogue stick
+                    x_axis, y_axis = joystick['lx', 'ly']
+                    # Set motor speeds
+                    print('x: ' + x_axis + ' y: ' + y_axis)
+                    # Get a ButtonPresses object containing everything that was pressed since the last
+                    # time around this loop.
+                    joystick.check_presses()
+                    # Print out any buttons that were pressed, if we had any
+                    if joystick.has_presses:
+                        print(joystick.presses)
+                    # If home was pressed, raise a RobotStopException to bail out of the loop
+                    # Home is generally the PS button for playstation controllers, XBox for XBox etc
+                    if 'home' in joystick.presses:
+                        raise RobotStopException()
+        except IOError:
+            # We get an IOError when using the ControllerResource if we don't have a controller yet,
+            # so in this case we just wait a second and try again after printing a message.
+            print('No controller found yet')
+            sleep(1)
+except RobotStopException:
+    # This exception will be raised when the home button is pressed, at which point we should
+    # stop the motors.
+    # stop_motors()
+    print('exitting ...')
